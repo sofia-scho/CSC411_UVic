1ebc681798aa9775eee29990d1d1025f3c306ce4
Sofia <sofia.scholefield@gmail.com>
2022-11-24 16:01:25 -0800

    bar

--- app.js
+++ app.js
@@ -610,7 +610,7 @@ d3.csv("https://raw.githubusercontent.com/Shake1999/CSC411_UVic/main/world_pover
                         str += 'Japan and South Africa.'
                 }
                 else {
-                    str += 'Haiti and ...'
+                    str += 'Haiti, Mexico, and Argentina'
                 }
                 countryTypeInfo.append('g')
                                 .append('text')
@@ -621,11 +621,161 @@ d3.csv("https://raw.githubusercontent.com/Shake1999/CSC411_UVic/main/world_pover
                                 .style('font-weight','bold')
                                 .text(str)
                                 .call(wrap,470);
+                
+                                var percent = 55;
+
+                                var w=300,h=320;
+                            
+                                var outerRadius=(w/2)-10;
+                                var innerRadius=outerRadius-8;
+                            
+                            
+                                var color = ['#ec1561','#2a3a46','#202b33'];
+                            
+                                var arc=d3.countryTypeInfo.arc()
+                                        .innerRadius(innerRadius)
+                                        .outerRadius(outerRadius)
+                                        .startAngle(0)
+                                        .endAngle(2*Math.PI);
+                            
+                                //The circle is following this
+                                var arcDummy=d3.countryTypeInfo.arc()
+                                        .innerRadius((outerRadius-innerRadius)/2+innerRadius)
+                                        .outerRadius((outerRadius-innerRadius)/2+innerRadius)
+                                        .startAngle(0);
+                            
+                            
+                                var arcLine=d3.svg.arc()
+                                        .innerRadius(innerRadius)
+                                        .outerRadius(outerRadius)
+                                        .startAngle(0);
+                            
+                                var svg=d3.select("#chart")
+                                        .append("svg")
+                                        .attr({
+                                            width:w,
+                                            height:h,
+                                            class:'shadow'
+                                        }).append('g')
+                                        .attr({
+                                            transform:'translate('+w/2+','+h/2+')'
+                                        });
+                            
+                            
+                                //background
+                                var path=svg.append('path')
+                                        .attr({
+                                            d:arc
+                                        })
+                                        .style({
+                                            fill:color[1]
+                                        });
+                            
+                            
+                                var pathForeground=svg.append('path')
+                                        .datum({endAngle:0})
+                                        .attr({
+                                            d:arcLine
+                                        })
+                                        .style({
+                                            fill:color[0]
+                                        });
+                            
+                                //Dummy Arc for Circle
+                                var pathDummy=svg.append('path')
+                                        .datum({endAngle:0})
+                                        .attr({
+                                            d:arcDummy
+                                        }).style({
+                                            fill:color[0]
+                                        });
+                            
+                                var endCircle=svg.append('circle')
+                                        .attr({
+                                            r:12,
+                                            transform:'translate(0,'+ (-outerRadius+15) +')'
+                                        })
+                                        .style({
+                                            stroke:color[0],
+                                            'stroke-width':8,
+                                            fill:color[2]
+                                        });
+                            
+                                var middleTextCount=svg.append('text')
+                                        .datum(0)
+                                        .text(function(d){
+                                            return d+'%';
+                                        })
+                            
+                                        .attr({
+                                            class:'middleText',
+                                            'text-anchor':'middle',
+                                            dy:25,
+                                            dx:0
+                                        })
+                                        .style({
+                                            fill:'#ec1561',
+                                            'font-size':'80px'
+                            
+                                        });
+                            
+                            
+                                var arcTweenOld=function(transition, percent,oldValue) {
+                                    transition.attrTween("d", function (d) {
+                            
+                                        var newAngle=(percent/100)*(2*Math.PI);
+                            
+                                        var interpolate = d3.interpolate(d.endAngle, newAngle);
+                            
+                                        var interpolateCount = d3.interpolate(oldValue, percent);
+                            
+                            
+                                        return function (t) {
+                                            d.endAngle = interpolate(t);
+                                            var pathForegroundCircle = arcLine(d);
+                            
+                                            middleTextCount.text(Math.floor(interpolateCount(t))+'%');
+                            
+                                            var pathDummyCircle = arcDummy(d);
+                            
+                                            console.log(pathDummyCircle);
+                            
+                                            var coordinate = pathDummyCircle.split("L")[1].split("A")[0];
+                            
+                                            console.log(coordinate);
+                            
+                                            endCircle.attr('transform', 'translate(' + coordinate+ ')');
+                            
+                                            return pathForegroundCircle;
+                                        };
+                                    });
+                                };  
+                               
+                                 
+                                var animate=function(){
+                                    pathChart.transition()
+                                            .duration(750)
+                                            .ease('cubic')
+                                            .call(arcTween,((2*Math.PI))*ratio, percent, oldValue);
+                                 
+                                 
+                                };
+                                 
+                                 
+                                setTimeout(animate,0);
+               
+
+                
 
             })
+            
             .on('mouseout', function(){
+                /*
                 d3.select('#countryTypeInformation')
                     .remove();
+                */
+                
             });
+            
     }
 );
